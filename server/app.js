8f48212ccc41b6167b0d80398662f63223ce022c
Maya Ausset <mayaausset@gmail.com>
2021-03-17 15:08:09 +0100

    favicon pb fixed

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const express = require("express");
-const favicon = require("serve-favicon");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const cors = require("cors");
@@ -46,7 +45,6 @@ app.use(
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
 app.locals.title = "React Weather App";
